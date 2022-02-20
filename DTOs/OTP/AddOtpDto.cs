56f0ec98a70c24c3cfc0f8db0e6a3a1e3d3ee965
AdrianChiritescu <47448789+AdrianChiritescu@users.noreply.github.com>
2022-02-20 23:11:22 +0200

    Added props to DTOs

--- DTOs/OTP/AddOtpDto.cs
+++ DTOs/OTP/AddOtpDto.cs
@@ -2,6 +2,7 @@ namespace OTP_generator.DTOs.OTP
 {
     public class AddOtpDto
     {
-
+        public string UserId { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; } = DateTime.Now;
     }
 }
--- DTOs/OTP/GetOtpDto.cs
+++ DTOs/OTP/GetOtpDto.cs
@@ -2,6 +2,7 @@ namespace OTP_generator.DTOs.OTP
 {
     public class GetOtpDto
     {
-
+        public string OTP { get; set; } = String.Empty;
+        public int ExpiresIn { get; set; } // Seconds
     }
 }
