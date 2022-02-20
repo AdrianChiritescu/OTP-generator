c3ef29793e3eef3751cac2f17210b1d8a320cbc7
AdrianChiritescu <47448789+AdrianChiritescu@users.noreply.github.com>
2022-02-21 00:41:05 +0200

    Added persistence layer with EF

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using OTP_generator.Models;
+
+namespace OTP_generator.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<OtpModel>? OTPs { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 using OTP_generator.Services;
+using OTP_generator.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
